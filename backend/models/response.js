58e492f04a70ebdfba7fe2484ac75f9f14cbb4ea
CharlesKasasira <charleskasasira01@gmail.com>
2021-12-01 09:16:25 +0300

    response data modeling

--- backend/models/response.js
+++ backend/models/response.js
@@ -4,5 +4,9 @@ const responseSchema = mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now 
+    },
+    questions_id: {
+        type: String,
+        required: true 
     }
 })
